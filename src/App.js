0c1475649bf406b04a8b2ad9013569c425052f72
MatthieuBP <mbp0651@gmail.com>
2021-10-20 20:24:04 -0700

    Added the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './pages';
+import FAQ from './pages/FAQ';
+import Contact from './pages/contact';
+import Account from './pages/account';
+import Mentions from './pages/mentions';
+import Sidebar from './components/Sidebar';
+import { FooterContainer } from './Containers/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/accueil" exact component={Home} />
+        <Route path="/faq" exact component={FAQ} />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/mon-compte" exact component={Account} />
+        <Route path="/mentions-legales" exact component={Mentions} />
+      </Switch>
+      <FooterContainer />
+    </Router>
   );
 }
 
